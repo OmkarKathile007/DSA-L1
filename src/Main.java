8866fb5619c340cd2863b1b735f8fd5012e315bf
Omkar Kathile <157679976+OmkarKathile007@users.noreply.github.com>
2024-10-30 16:12:37 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
 public class Main {
+    /// Sieves of Erastothenes
+    public static void Tprimes{
+        int N=1000001;
+
+        boolean[] arr = new boolean[N];
+
+        for (int i = 2; i * i < N; i++) {
+            if (!arr[i]) {
+                for (int j = i * i; j < N; j += i) {
+                    arr[j] = true;
+                }
+            }
+        }
+
+        Set<Long> res = new HashSet<>();
+        for (int i = 2; i < N; i++) {
+            if (!arr[i]) {
+                res.add((long) i * i);
+            }
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
