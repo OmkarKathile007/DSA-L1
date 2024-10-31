f5f8d3d1f0edc8a8332fed4e3fb95ef32172cf76
Omkar Kathile <157679976+OmkarKathile007@users.noreply.github.com>
2024-10-31 13:48:13 +0530

    Update Main.java
    Added Number Theory Content

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,42 @@ public class Main {
         }
 
     }
+     public static int gcd(int a,int b){
+
+        while(b!=0){
+            int t=b;
+            b=a%b;
+            a=t;
+        }
+
+        return a;
+    }
+    public static int gcd1(int a,int b){
+
+       if(b==0) return a;
+
+        return gcd1(b,a%b);
+    }
+//    public static final int MOD=100005;
+
+    public static final int MOD = 1000000007; // Set MOD to your desired modulus
+
+    public static int BinaryExpo(int a, int b) {
+        int temp = 1;
+        if (b == 0) {
+            return 1;
+        }
+        temp = BinaryExpo(a, b / 2);
+        temp %= MOD;
+        temp *= temp;
+        temp %= MOD;
+        if ((b & 1) == 1) { // if b is odd
+            temp *= a;
+            temp %= MOD; // fix here
+        }
+        return temp;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
